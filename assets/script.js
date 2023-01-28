09b5b249078281e6249a9a247f8ba9a063ed71f0
Kinori <russel.richard@outlook.com>
2023-01-28 18:02:26 +0000

    added comments and pseudocode to js file

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,70 @@
 // Create an object for the api key 
-
+let searchResults = {};
 //Create a function for buiding the url
+let apiKey = "3703dc0a87a12ecb40acb1b87b0e9d16";
 function buidUrl() {
     let initialUrl = "https://api.openweathermap.org/geo/1.0/direct?q=";
-    let apiKey = "3703dc0a87a12ecb40acb1b87b0e9d16";
     let q = $("#search-input").val().trim();
     console.log("---------------\nURL: " + initialUrl + "\n---------------");
 
     //https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=3703dc0a87a12ecb40acb1b87b0e9d16
-    fetch(initialUrl + q + "&limit=6&appid=" + apiKey)
+    fetch(initialUrl + q + "&limit=5&appid=" + apiKey)
         .then(response => response.json())
         .then(citiesFound => {
-            console.log(citiesFound);
-            let city = citiesFound[0];
-            console.log(city.lat);
-            console.log(city.lon);
+            for (let i = 0; i < citiesFound.length; i++) {
+                const city = citiesFound[i];
 
-            return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=${apiKey}api`)
+                console.log(citiesFound);
+                // let city1 = citiesFound[i];
+                // console.log(city.lat);
+                // console.log(city.lon);
+                //https://api.openweathermap.org/data/2.5/forecast?lat=35.6828387&lon=${city.lon}&appid=${apiKey}
+                return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=${apiKey}`)
+            }
         })
 
-        .then(response => response.json())
-        .then(results => {
-            let thisResults = results[0]
-            console.log(thisResults);
+        .then((response) => response.json())
+        .then((data) => {
+            searchResults = data;
+            // console.log(searchResults);
             // searchResults.push(results)
         })
+
+
 }
-//Fetch for when the button is clicked
+
+//Function to display results on the webpage
+function updatePage() {
+    let data = buidUrl(data);
+    console.log(data)
+    let todaysWeather = $("#today");
+    let forecast = $("#forecast")();
+
+    // temp1.city.name
+    // temp1.list[0].dt_txt
+    // temp1.list[0].main.temp
+    // temp1.list[0].wind.speed
+    // temp1.list[0].main.humidity
+    // console.log(results);
+}
+
 //Grab the value from the button rather than the input
 //use event delegation (event.target.textContent) to grab the text of the city
 //use icons from the fetch data
 
+//Fetch for when the button is clicked
 //Create a function to use the search button on click
 $("#search-button").on("click", function (event) {
-    // This line allows us to take advantage of the HTML "submit" property
-    // This way we can hit enter on the keyboard and it registers the search
-    // (in addition to clicks). Prevents the page from reloading on form submit.
-    event.preventDefault();
 
-    // Empty the region associated with the articles
-    // clear();
+    event.preventDefault();    //Prevent the page from reloading on form submit.
 
-    // Build the query URL for the ajax request to the NYT API
-    var searchWeather = buidUrl();
+    let searchWeather = buidUrl();  // Build the query URL for the ajax request to the weather API
 
-    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-    // The data then gets passed as an argument to the updatePage function
+    // Make the AJAX request to the API - GETs the JSON data at the buidUrl.
     $.ajax({
         url: searchWeather,
         method: "GET"
-    }).then();
+    })//.then(weatherDisplay);  // The data then gets passed as an argument to the weatherDisplay function
 });
 
 //  .on("click") function associated with the clear button
