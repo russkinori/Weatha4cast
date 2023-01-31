0702f829d21a093f524aa8b09992331fbd4f7c7c
Kinori <russel.richard@outlook.com>
2023-01-31 23:27:01 +0000

    added an asset and a images folder. added a stylesheet. added a div element to the html file. added comments to the js file

--- assets/script.js
+++ assets/script.js
@@ -1,86 +1,15 @@
-// Create an object for the api key 
-let searchResults = {};
-//Create a function for buiding the url
-let apiKey = "3703dc0a87a12ecb40acb1b87b0e9d16";
-function buidUrl() {
-    let initialUrl = "https://api.openweathermap.org/geo/1.0/direct?q=";
-    let q = $("#search-input").val().trim();
-    console.log("---------------\nURL: " + initialUrl + "\n---------------");
-
-    //https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=3703dc0a87a12ecb40acb1b87b0e9d16
-    fetch(initialUrl + q + "&limit=5&appid=" + apiKey)
-        .then(response => response.json())
-        .then(citiesFound => {
-            for (let i = 0; i < citiesFound.length; i++) {
-                const city = citiesFound[i];
-
-                console.log(citiesFound);
-                // let city1 = citiesFound[i];
-                // console.log(city.lat);
-                // console.log(city.lon);
-                //https://api.openweathermap.org/data/2.5/forecast?lat=35.6828387&lon=${city.lon}&appid=${apiKey}
-                return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=${apiKey}`)
-            }
-        })
-
-        .then((response) => response.json())
-        .then((data) => {
-            searchResults = data;
-            // console.log(searchResults);
-            // searchResults.push(results)
-        })
-
-
-}
-
-//Function to display results on the webpage
-function updatePage() {
-    let data = buidUrl(data);
-    console.log(data)
-    let todaysWeather = $("#today");
-    let forecast = $("#forecast")();
-
-    // temp1.city.name
-    // temp1.list[0].dt_txt
-    // temp1.list[0].main.temp
-    // temp1.list[0].wind.speed
-    // temp1.list[0].main.humidity
-    // console.log(results);
-}
-
-//Grab the value from the button rather than the input
-//use event delegation (event.target.textContent) to grab the text of the city
-//use icons from the fetch data
-
-//Fetch for when the button is clicked
-//Create a function to use the search button on click
-$("#search-button").on("click", function (event) {
-
-    event.preventDefault();    //Prevent the page from reloading on form submit.
-
-    let searchWeather = buidUrl();  // Build the query URL for the ajax request to the weather API
-
-    // Make the AJAX request to the API - GETs the JSON data at the buidUrl.
-    $.ajax({
-        url: searchWeather,
-        method: "GET"
-    })//.then(weatherDisplay);  // The data then gets passed as an argument to the weatherDisplay function
-});
-
-//  .on("click") function associated with the clear button
-// $("#clear-all").on("click", clear);
-// console.log(searchResults);
-
-// renderSearch();
-
-// let searchTerm = $("#search-button")
-
-// searchTerm.addEventListener("click", function (event) {
-
-
-//     // onclick = 'document.getElementById("demo").innerHTML = "New content!"
-//     if (event.target.matches("button")) {
-//         let city = event.target.textContent;
-//         console.log(city);
-//     }
-// })
+// Create a weather dashboard with form inputs.
+// When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history
+// When a user views the current weather conditions for that city they are presented with:
+// The city name
+// The date
+// An icon representation of weather conditions
+// The temperature
+// The humidity
+// The wind speed
+// When a user view future weather conditions for that city they are presented with a 5-day forecast that displays:
+// The date
+// An icon representation of weather conditions
+// The temperature
+// The humidity
+// When a user click on a city in the search history they are again presented with current and future conditions for that city
